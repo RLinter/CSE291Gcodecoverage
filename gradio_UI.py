edbd9dda5d025ca096c96b818e3b4b2a9d0cf3a1
I1amanoutsider <1155141574@link.cuhk.edu.hk>
2024-05-20 22:31:24 -0700

    init UI script and readme

---
+++ gradio_UI.py
@@ -0,0 +1,51 @@
+
+
+import argparse
+import sys
+import os
+import random
+
+from datetime import datetime
+from typing import List, Union
+import gradio as gr
+import numpy as np
+from gradio.components import Textbox, Video, Image
+from openai import OpenAI
+client = OpenAI(api_key="sk-proj-2bukD0FHxc5cMV1KaW90T3BlbkFJ6nfZBZlPJc9SnImkUysE")
+
+
+def generate_testcase(prompt):
+    code = {1: 'from dataclasses import dataclass', 2: '', 3: '@dataclass()', 4: 'class Product:', 5: '', 6: '    id: int', 7: '    name: str', 8: '    price: float', 9: '    stock: int', 10: '', 11: '    def increase_stock(self, stock_to_add: int):', 12: '        self.check_positive_number(stock_to_add)', 13: '        self.stock: int = self.stock + stock_to_add', 14: '', 15: '    def decrease_stock(self, stock_to_reduce):', 16: '        self.check_positive_number(stock_to_reduce)', 17: '        new_stock = self.stock - stock_to_reduce', 18: '        self.check_negative_stock(new_stock)', 19: '        self.stock = self.stock - stock_to_reduce', 20: '', 21: '    def check_positive_number(self, value):', 22: '        if value <= 0:', 23: '            raise Exception("Number must be positive")', 24: '', 25: '    def check_negative_stock(self, value):', 26: '        if value < 0:', 27: '            raise Exception("Stock must be greater than or equal to 0")', 28: ''}
+    gpt_prompt = "Given the following code, generate test cases. Only respond with code that is valid Python code starting with code: from main import Product from dataclasses import is_dataclass import pytest @pytest.fixture def product(): return Product(1, 'foo', 1.0, 10) def test_constructor(product): assert product.id == 1 assert product.name == 'foo'"
+    message=[{"role": "system", "content": "You are a helpful programmer writing test.py."},{"role": "assistant", "content": gpt_prompt + "\n" + "\n".join(code.values())}]
+
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages = message,
+    )
+    responseText = response.choices[0].message.content
+    # get rid of the first 10 characters and the last 3 characters
+    responseText = responseText[10:]
+    responseText = responseText[:-3]
+    responseText
+    
+    return responseText, code
+
+if __name__ == '__main__':
+
+    demo = gr.Interface(
+        fn=generate_testcase,
+        inputs=[Textbox(label="Enter you coder",
+                        placeholder="Please enter your code. \n"),
+                ],
+        outputs=[
+                #  Video(label="Vid", width=512, height=512),
+                 Textbox(label="generated test cases"),
+                 Textbox(label="test case coverage report"),
+                #  gr.Slider(label='seed')
+                 ],
+        # title=title_markdown, description=DESCRIPTION, theme=gr.themes.Default(), css=block_css,
+        # examples=examples,
+    )
+    demo.launch()
---
+++ readme.md
@@ -0,0 +1,15 @@
+## LLM integrate test coverage checker 
+
+### Go throught the workflow step by step
+run experiment.ipynb step by step
+
+
+### Get gradio UI ready in your environment
+```
+pip install gradio
+```
+### Activate server
+```
+python ./gradio_UI.py
+```
+use local address http://127.0.0.1:7860/ to open the server
