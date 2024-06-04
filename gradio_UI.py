f05246f96677361ff631f6928e8ce2837daf170f
rlsu9 <r3su@ucsd.edu>
2024-06-03 18:22:18 -0700

    modify UI and avoid hardcode

--- gradio_UI.py
+++ gradio_UI.py
@@ -10,11 +10,15 @@ from datetime import datetime
 from typing import List, Union
 import gradio as gr
 import numpy as np
-from gradio.components import Textbox, Video, Image, HTML
+from gradio.components import Textbox, Video, Image, HTML, File
 from openai import OpenAI
 import difflib
 import coverage
 client = OpenAI(api_key="sk-proj-2bukD0FHxc5cMV1KaW90T3BlbkFJ6nfZBZlPJc9SnImkUysE")
+
+def read_file_as_string(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
 
 def get_code_coverage(code, test_case):
     # Create directory tmp, main.py and test_main.py 
@@ -39,7 +43,7 @@ def get_code_coverage(code, test_case):
     cov = coverage.Coverage()
     cov.load()
     coverage_percentage = 100 - 100 * (len(cov.analysis('tmp/main.py')[2]) / len(cov.analysis('tmp/main.py')[1]))
-    return str(round(coverage_percentage, 2)) + '%'
+    return str(round(coverage_percentage, 2)) + '%', cov.analysis('tmp/main.py')[2]
 
 def extract_code_from_response(response):
     pattern = r"```python(.*?)```"
@@ -91,7 +95,7 @@ def generate_prompt(user_code, user_test_cases, coverage_report):
     return prompt
 
 # The main workflow for the server
-def generate_testcase(user_code, user_test_case):
+def generate_testcase(user_code, user_test_case, code_files, test_files):
 
     # FIXME: comment these part out for live input
     # file_user_code_path = 'main.py'
@@ -104,15 +108,28 @@ def generate_testcase(user_code, user_test_case):
     # take the prompt and test case as input from the user
 
     #TODO subtask 2: organize the user input into reasonable llm prompt as following format
-    code = {1: 'from dataclasses import dataclass', 2: '', 3: '@dataclass()', 4: 'class Product:', 5: '', 6: '    id: int', 7: '    name: str', 8: '    price: float', 9: '    stock: int', 10: '', 11: '    def increase_stock(self, stock_to_add: int):', 12: '        self.check_positive_number(stock_to_add)', 13: '        self.stock: int = self.stock + stock_to_add', 14: '', 15: '    def decrease_stock(self, stock_to_reduce):', 16: '        self.check_positive_number(stock_to_reduce)', 17: '        new_stock = self.stock - stock_to_reduce', 18: '        self.check_negative_stock(new_stock)', 19: '        self.stock = self.stock - stock_to_reduce', 20: '', 21: '    def check_positive_number(self, value):', 22: '        if value <= 0:', 23: '            raise Exception("Number must be positive")', 24: '', 25: '    def check_negative_stock(self, value):', 26: '        if value < 0:', 27: '            raise Exception("Stock must be greater than or equal to 0")', 28: ''}
-    test_code = {1: 'from main import Product', 2: 'from dataclasses import is_dataclass', 3: 'import pytest', 4: '', 5: 'def test_if_it_is_a_dataclass():', 6: '    assert is_dataclass(Product) == True', 7: '', 8: '', 9: '@pytest.fixture', 10: 'def product():', 11: "    return Product(1, 'foo', 1.0, 10)", 12: '', 13: '', 14: 'def test_constructor(product):', 15: '    assert product.id == 1', 16: "    assert product.name == 'foo'", 17: '    assert product.price == 1.0', 18: '    assert product.stock == 10', 19: '', 20: '', 21: 'def test_increase_stock(product):', 22: '    product.increase_stock(10)', 23: '    assert product.stock == 20'}
-    missing_lines = {16: '        self.check_positive_number(stock_to_reduce)', 17: '        new_stock = self.stock - stock_to_reduce', 18: '        self.check_negative_stock(new_stock)', 19: '        self.stock = self.stock - stock_to_reduce', 23: '            raise Exception("Number must be positive")', 26: '        if value < 0:', 27: '            raise Exception("Stock must be greater than or equal to 0")'}
+    if user_code:
+        code = {i+1: line for i, line in enumerate(user_code.split('\n'))}
+        test_code = {i+1: line for i, line in enumerate(user_test_case.split('\n'))}
+    else:
+        code = [{i+1: line for i, line in enumerate(read_file_as_string(code_file).split('\n'))} for code_file in code_files][0]
+        test_code = [{i+1: line for i, line in enumerate(read_file_as_string(test_file).split('\n'))} for test_file in test_files][0]
+
+    pred_coverage_report, missing_lines_list = get_code_coverage(code, test_code)
+    missing_lines = {}
+    for key, value in code.items():
+        if key in missing_lines_list:
+            missing_lines[key] = value
+
+
+    # code = {1: 'from dataclasses import dataclass', 2: '', 3: '@dataclass()', 4: 'class Product:', 5: '', 6: '    id: int', 7: '    name: str', 8: '    price: float', 9: '    stock: int', 10: '', 11: '    def increase_stock(self, stock_to_add: int):', 12: '        self.check_positive_number(stock_to_add)', 13: '        self.stock: int = self.stock + stock_to_add', 14: '', 15: '    def decrease_stock(self, stock_to_reduce):', 16: '        self.check_positive_number(stock_to_reduce)', 17: '        new_stock = self.stock - stock_to_reduce', 18: '        self.check_negative_stock(new_stock)', 19: '        self.stock = self.stock - stock_to_reduce', 20: '', 21: '    def check_positive_number(self, value):', 22: '        if value <= 0:', 23: '            raise Exception("Number must be positive")', 24: '', 25: '    def check_negative_stock(self, value):', 26: '        if value < 0:', 27: '            raise Exception("Stock must be greater than or equal to 0")', 28: ''}
+    # test_code = {1: 'from main import Product', 2: 'from dataclasses import is_dataclass', 3: 'import pytest', 4: '', 5: 'def test_if_it_is_a_dataclass():', 6: '    assert is_dataclass(Product) == True', 7: '', 8: '', 9: '@pytest.fixture', 10: 'def product():', 11: "    return Product(1, 'foo', 1.0, 10)", 12: '', 13: '', 14: 'def test_constructor(product):', 15: '    assert product.id == 1', 16: "    assert product.name == 'foo'", 17: '    assert product.price == 1.0', 18: '    assert product.stock == 10', 19: '', 20: '', 21: 'def test_increase_stock(product):', 22: '    product.increase_stock(10)', 23: '    assert product.stock == 20'}
+    # missing_lines = {16: '        self.check_positive_number(stock_to_reduce)', 17: '        new_stock = self.stock - stock_to_reduce', 18: '        self.check_negative_stock(new_stock)', 19: '        self.stock = self.stock - stock_to_reduce', 23: '            raise Exception("Number must be positive")', 26: '        if value < 0:', 27: '            raise Exception("Stock must be greater than or equal to 0")'}
     code_txt = "\n".join(code.values())
     test_code_txt = "\n".join(test_code.values())
     missing_lines_txt = "\n".join(missing_lines.values())
     #TODO subtask 1: use the current test case and code to generate a test coverage report, store it as pred_coverage_report
     # pred_coverage_report = subprocess.run("some command to get coverage")
-    pred_coverage_report = get_code_coverage(code, test_code)
 
 
     gpt_prompt = "Given the following code, existing test cases, and lines missing test coverage, generate more test cases. Make sure the test cases are valid and increase test coverage. Only respond with valid Python code, do not return explanation."
@@ -138,7 +155,7 @@ def generate_testcase(user_code, user_test_case):
     
     #TODO subtask 4: after receive the feedback from llm, organize it into a machine processable format and use model to check the updated test case coverage, store as succ_coverage_report
     # succ_coverage_report = subprocess.run("some command to get coverage")
-    succ_coverage_report = get_code_coverage(code, generated_code)
+    succ_coverage_report, _ = get_code_coverage(code, generated_code)
     #TODO subtask 5: highlight the output shown in the UI to tell the user what difference we made. (new test we added, previous test case we modified, test coverage changes)
     responseText = highlight_differences(test_code_txt, responseText)
     return responseText, pred_coverage_report, succ_coverage_report
@@ -150,6 +167,8 @@ if __name__ == '__main__':
                         placeholder="Please enter your code. \n"),
                 Textbox(label="Enter you test case",
                         placeholder="Please enter your test case. \n"),
+                File(label="Upload your coding files", file_count="multiple", type="filepath"),
+                File(label="Upload your testing files", file_count="multiple", type="filepath")
                 ],
         outputs=[
                 #  Video(label="Vid", width=512, height=512),
